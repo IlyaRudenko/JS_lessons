c23cc3ae0398d089f76275164040a7fa2024b175
IlyaRudenko <Rudenko123456@gmail.com>
2022-11-02 19:42:14 +0200

    JSHW1

---
+++ Lesson1HW/JS.js
@@ -0,0 +1,66 @@
+let ab
+ab = 'hello'
+console.log(ab)
+
+let bc = 'owu'
+console.log(bc)
+
+let cd='com'
+console.log(cd)
+
+let d= 'ua'
+console.log(d)
+
+let e = 1
+console.log(e)
+
+let f = 10
+console.log(f)
+
+let g = -999
+console.log(g)
+
+let h = 123
+console.log(h)
+
+const PI = 3.14
+console.log(PI)
+
+let i = 2.7
+console.log(i)
+
+let j = 16
+console.log(j)
+
+let k = true
+console.log(k)
+
+let l = false
+console.log(l)
+
+
+
+
+let firstname = 'Ilya'
+let middlename = 'Rudenko'
+let lastname = 'Andriyovich'
+
+let person = firstname + ' ' + middlename + ' ' + lastname
+console.log(person)
+
+let person2 = `${firstname} ${middlename} ${lastname}`
+console.log(person2)
+
+
+
+
+let a = 100
+console.log(typeof a)
+let b = 100
+console.log(l=typeof b)
+let c = true
+console.log(typeof c)
+
+
+
+
