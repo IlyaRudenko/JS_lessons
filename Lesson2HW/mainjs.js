389b59157e83d765fb57f6c559cf34edd2c37da3
IlyaRudenko <Rudenko123456@gmail.com>
2022-11-06 20:08:49 +0200

    JSHW1

---
+++ Lesson2HW/mainjs.js
@@ -0,0 +1,103 @@
+
+//- Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+let arr = [123, 232, 6454, 456, 134, 12353, 67, 67856, 69, 464]
+console.log(arr)
+console.log(arr [0])
+console.log(arr[1])
+console.log(arr[2])
+console.log(arr[3])
+console.log(arr[4])
+console.log(arr[5])
+console.log(arr[6])
+console.log(arr[7])
+console.log(arr[8])
+console.log(arr[9])
+
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+let book = {
+    title: 'okorok',
+    pagecount: 222,
+    genre: 'comedy'
+}
+console.log(book)
+
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - масив. Кожен автор має поля name,age
+let books = {
+    title: 'noname',
+    pagecount: 333,
+    genre: 'fantasy',
+    author: [
+        {name: 'Boris',
+        age: 28},
+        {name: 'Vasya',
+        age: 36}
+    ]
+}
+console.log(books)
+
+
+// - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача*/
+let samsa = [
+    {
+    name: 'roma',
+    username: 'romeo',
+    password: 100
+    },
+    {
+        name: 'roma1',
+        username: 'romeo1',
+        password: 101
+    },
+    {
+        name: 'roma2',
+        username: 'romeo2',
+        password: 102
+    },
+    {
+        name: 'roma3',
+        username: 'romeo3',
+        password: 103
+    },
+    {
+        name: 'roma4',
+        username: 'romeo4',
+        password: 104
+    },
+    {
+        name: 'roma5',
+        username: 'romeo5',
+        password: 105
+    },
+    {
+        name: 'roma6',
+        username: 'romeo6',
+        password: 106
+    },
+    {
+        name: 'roma7',
+        username: 'romeo7',
+        password: 107
+    },
+    {
+        name: 'roma8',
+        username: 'romeo8',
+        password: 108
+    },
+    {
+        name: 'roma9',
+        username: 'romeo9',
+        password: 109
+    }
+]
+console.log(samsa[0].password)
+console.log(samsa[1].password)
+console.log(samsa[2].password)
+console.log(samsa[3].password)
+console.log(samsa[4].password)
+console.log(samsa[5].password)
+console.log(samsa[6].password)
+console.log(samsa[7].password)
+console.log(samsa[8].password)
+console.log(samsa[9].password)
